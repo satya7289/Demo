c4a7b706e4103cd8a61b83c89093e8ead0ccba89
satya7289 <satyaprakashaman60@gmail.com>
2019-09-26 17:45:09 +0530

    adding javascript for validations

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -87,29 +87,27 @@
                     <div class="form-group row">
                         <label for="name" class="col-sm-2 col-form-label">Name:</label>
                         <div class="col-sm-10">
-                            <input class="form-control" name="name" id="name" type="text" placeholder="Enter Name" aria-describedby="nameHelpText">
-                            @error('name')
+                            <input class="form-control" name="name" id="name" type="text" placeholder="Enter Name" aria-describedby="nameHelpText" onkeyup="nameValidate(event)"  required>
                             <small id="nameHelpText" class="form-text text-muted">
-                                {{ $message }}
+                            @error('name') {{ $message }} @enderror
                             </small>
-                            @enderror
+                            
                         </div>
                     </div>
                     <div class="form-group row">
                         <label for="email" class="col-sm-2 col-form-label">Email</label>
                         <div class="col-sm-10">
-                            <input class="form-control" name="email" type="email" id="email" placeholder="Enter Email" aria-describedby="emailHelpText">
-                            @error('email')
+                            <input class="form-control" name="email" type="email" id="email" placeholder="Enter Email" aria-describedby="emailHelpText" required onkeyup="emailValidate()">
+                            
                             <small id="emailHelpText" class="form-text text-muted">
-                                {{ $message }}
+                                
                             </small>
-                            @enderror
                         </div>
                     </div>
                     <div class="form-group row">
                         <label for="pincode" class="col-sm-2 col-form-label">Pincode</label>
                         <div class="col-sm-10">
-                            <input class="form-control" name="pincode" id="pincode" min="100000" max="999999" type="number" placeholder="Enter Pincode" aria-describedby="pincodeHelpText" >
+                            <input class="form-control" name="pincode" id="pincode" min="100000" max="999999" type="number" placeholder="Enter Pincode" aria-describedby="pincodeHelpText" required onkeyup="pincodeValidate()">
                             <small id="pincodeHelpText" class="form-text text-muted">
                                 Pincode must be 6 digit length, from 100000 to 999999.<br>
                                 @error('pincode')
@@ -123,6 +121,71 @@
                 </form>
             </div>
         </div>
+
+
+
+
+    <script>
+        function emailValidate(){
+            var patt = new RegExp("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$");
+            var email = document.getElementById('email');
+            var value = email.value;
+            if(patt.test(value))
+            {
+                document.getElementById('emailHelpText').innerText=""
+                email.style.backgroundColor = "#00e673";
+                email.classList.remove('is-invalid');
+                email.classList.add('is-valid');
+
+            }
+            else{
+                email.style.backgroundColor = "#ff6666";
+                document.getElementById('emailHelpText').innerText="Enter Valid email";
+                email.classList.remove('is-valid');
+                email.classList.add('is-invalid');
+
+            }
+        }
+        function nameValidate(e){
+            var name = document.getElementById('name');
+            var patt = new RegExp("^[a-zA-Z][a-zA-Z0-9]*$");
+            var value = name.value;
+            if(value.toString().length>0 && patt.test(value))
+            {
+                document.getElementById('nameHelpText').innerText=""
+                name.style.backgroundColor = "#00e673";
+                name.classList.remove('is-invalid');
+                name.classList.add('is-valid');
+            }
+            else{
+                name.style.backgroundColor = "#ff6666";
+                document.getElementById('nameHelpText').innerText="Enter Name";
+                name.classList.remove('is-valid');
+                name.classList.add('is-invalid');
+            }
+        }
+        function pincodeValidate(){
+            var pincode = document.getElementById('pincode');
+            var value = pincode.value;
+            if(value.length==6 && value>0)
+            {
+                document.getElementById('pincodeHelpText').innerText=""
+                pincode.style.backgroundColor = "#00e673";
+                pincode.classList.remove('is-invalid');
+                pincode.classList.add('is-valid');
+            }
+            else{
+                pincode.style.backgroundColor = "#ff6666";
+                document.getElementById('pincodeHelpText').innerText="Enter Pincode of 6 digit.";
+                pincode.classList.remove('is-valid');
+                pincode.classList.add('is-invalid');
+            }
+        }
+        
+    </script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     </body>
 
 
